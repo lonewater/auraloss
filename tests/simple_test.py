09fce0ae602cb45d9c23b1218306993619dc26a2
lonewater <andrew.parker@hud.ac.uk>
2022-05-28 21:58:54 +0100

    finished testing percpt filters

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -5,12 +5,12 @@ input = torch.rand(8, 2, 44100)
 target = torch.rand(8, 2, 44100)
 
 # loss = auraloss.freq.SumAndDifferenceSTFTLoss()
-# loss = auraloss.freq.PerceptuallyWeightedComplexLoss()
+loss = auraloss.freq.PerceptuallyWeightedComplexLoss()
 
-# print(loss(input, target))
+print(loss(input, target))
 
 # filter = auraloss.freq.FIRFilter(filter_type="r468w", plot=True)
-filter = auraloss.freq.FIRFilter(filter_type="cw", plot=True)
+# filter = auraloss.freq.FIRFilter(filter_type="cw", plot=True)
 # filter = auraloss.freq.FIRFilter(filter_type="aw", plot=True)
 
-test = filter(input, target)
+# test = filter(input, target)

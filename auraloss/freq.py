26943c4b32b20d86b22a17e34d6f14f058dc80bd
lonewater <andrew.parker@hud.ac.uk>
2022-06-03 21:21:33 +0100

    fix mean and sum calls

--- auraloss/freq.py
+++ auraloss/freq.py
@@ -707,10 +707,10 @@ class PerceptuallyWeightedComplexLoss(torch.nn.Module):
         melSumWeight = 519 / (140 * (1 + fc / 700) * np.log(10)) # derrivative of mel frequency curve can be used as compensatory weigting function for summing/averaging across bins
         melSumWeight = melSumWeight.reshape(1, len(melSumWeight), 1) # sure up axes to allow broadcast in next line
 
-        rowMean = np.mean(euDif * melSumWeight, axis=1) # mean over bins so to compensate for bin count
+        rowMean = (euDif * melSumWeight).mean(axis=1) # mean over bins so to compensate for bin count
         if self.reduction == "mean":
-            return np.mean(rowMean, axis=1) # mean over time steps/frames
+            return rowMean.mean(axis=1) # mean over time steps/frames
         elif self.reduction == "sum":
-            return np.sum(rowMean, axis=1) # sum over time steps/frames
+            return rowMean.sum(axis=1) # sum over time steps/frames
         elif self.reduction == None:
             return rowMean

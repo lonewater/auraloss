8e56b48924ae30c10f25595d74f2e3965eec1de0
lonewater <andrew.parker@hud.ac.uk>
2022-06-17 03:10:27 +0100

    Update simple_test.py

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -6,12 +6,12 @@ target = torch.rand(8, 2, 44100)
 
 # loss = auraloss.freq.SumAndDifferenceSTFTLoss()
 # loss = auraloss.freq.PerceptuallyWeightedComplexLoss()
-loss = auraloss.freq.MultiResolutionPrcptWghtdCmplxLoss()
+# loss = auraloss.freq.MultiResolutionPrcptWghtdCmplxLoss()
 
-print(loss(input, target))
+# print(loss(input, target))
 
-# filter = auraloss.freq.FIRFilter(filter_type="r468w", plot=True)
+filter = auraloss.freq.FIRFilter(filter_type="r468w", plot=True)
 # filter = auraloss.freq.FIRFilter(filter_type="cw", plot=True)
 # filter = auraloss.freq.FIRFilter(filter_type="aw", plot=True)
 
-# test = filter(input, target)
+test = filter(input, target)

318e97bc73af098613c861128c6ee0ed8a8b3168
lonewater <andrew.parker@hud.ac.uk>
2022-09-15 22:43:29 +0100

    Update simple_test.py

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -10,6 +10,6 @@ target = torch.rand(8, 2, 44100)
 
 # print(loss(input, target))
 
-filter = auraloss.freq.FIRFilter(filter_type="r468w", plot=True, ntaps=1001, inverse=True)
+filter = auraloss.freq.FIRFilter(filter_type="aw", plot=True, ntaps=1001, inverse=True)
 
 test = filter(input)
